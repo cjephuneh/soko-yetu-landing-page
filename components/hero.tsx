c7cf70eaca98376624254e649c4d7c5f9fa13246
cjephuneh <calebjephuneh@gmail.com>
2023-12-15 00:35:25 +0300

    upadtes

--- components/hero.tsx
+++ components/hero.tsx
@@ -56,14 +56,13 @@ export default function Example() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
                   src="https://flowbite.com/docs/images/logo.svg"
                   alt=""
                 />
               </a>
-              <span className="self-center text-2xl font-semibold whitespace-nowrap">Soko Yetu</span>
+              <span className="self-center text-2xl  text-black font-semibold whitespace-nowrap">Soko Yetu</span>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
