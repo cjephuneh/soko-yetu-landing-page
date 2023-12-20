11de2a84292f4d33d24f1a9982195606815da3e7
cjephuneh <calebjephuneh@gmail.com>
2023-12-20 13:12:46 +0300

    updates

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -19,11 +20,12 @@ export default function Example() {
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1 space-x-2">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
+              <Image
                 className="h-8 w-auto"
                 src="https://flowbite.com/docs/images/logo.svg"
                 alt=""
+                width={50}
+                height={50}
               />
             </a>
             <span className="self-center text-2xl text-black font-semibold whitespace-nowrap">Soko Yetu</span>
@@ -56,10 +58,12 @@ export default function Example() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <img
+                <Image
                   className="h-8 w-auto"
                   src="https://flowbite.com/docs/images/logo.svg"
                   alt=""
+                  width={50}
+                  height={50}
                 />
               </a>
               <span className="self-center text-2xl  text-black font-semibold whitespace-nowrap">Soko Yetu</span>
@@ -112,10 +116,10 @@ export default function Example() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-20 sm:py-20">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
+              Announcing our next round of Updates.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -130,7 +134,7 @@ export default function Example() {
             
             <p className="mt-6 text-lg leading-8 text-gray-600">
               We have in mind how expensive it is to hire developers to create an ecommerce store for your business. 
-              We have created a this platform so you focus only on your customers..
+              We have created a this platform so you focus only on your customers and not on the cost of hiring developers.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
