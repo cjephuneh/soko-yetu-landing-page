c8538d8a67544c62d1d6a41fd1d34787eeaf9c13
cjephuneh <calebjephuneh@gmail.com>
2023-12-21 21:36:23 +0300

    updates

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -16,27 +16,29 @@ interface PricingTabProps {
 
 function PricingTab(props: PricingTabProps) {
   return (
-    <div className={`h-full ${props.popular ? 'dark' : ''}`}>
-      <div className="relative flex flex-col h-full p-6 rounded-2xl bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-900 shadow shadow-slate-950/5">
+    
+    <div className=" h-full">
+      <div className="relative flex flex-col h-full p-6 rounded-2xl bg-gray-200  border border-slate-200 ">
         {props.popular && (
           <div className="absolute top-0 right-0 mr-6 -mt-4">
             <div className="inline-flex items-center text-xs font-semibold py-1.5 px-3 bg-emerald-500 text-white rounded-full shadow-sm shadow-slate-950/5">Most Popular</div>
           </div>
         )}
-        <div className="mb-5">
-          <div className="text-slate-900 dark:text-slate-200 font-semibold mb-1">{props.planName}</div>
+        <div className="mb-5 ">
+          <div className="text-slate-900  font-semibold mb-1">{props.planName}</div>
           <div className="inline-flex items-baseline mb-2">
-            <span className="text-slate-900 dark:text-slate-200 font-bold text-3xl">$</span>
-            <span className="text-slate-900 dark:text-slate-200 font-bold text-4xl">{props.yearly ? props.price.yearly : props.price.monthly}</span>
+            <span className="text-slate-900  font-bold text-3xl">$</span>
+            <span className="text-slate-90 font-bold text-4xl">{props.yearly ? props.price.yearly : props.price.monthly}</span>
             <span className="text-slate-500 font-medium">/mo</span>
           </div>
           <div className="text-sm text-slate-500 mb-5">{props.planDescription}</div>
-          <a className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150" href="#0">
+          <p className='text-gray-500 mb-2'> Try free for 14 days</p>
+          <a className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring " href="#0">
             Purchase Plan
           </a>
         </div>
-        <div className="text-slate-900 dark:text-slate-200 font-medium mb-3">Includes:</div>
-        <ul className="text-slate-600 dark:text-slate-400 text-sm space-y-3 grow">
+        <div className="text-slate-900  font-medium mb-3">Includes:</div>
+        <ul className="text-slate-600 text-sm space-y-3 grow">
           {props.features.map((feature, index) => {
             return (
               <li key={index} className="flex items-center">
@@ -48,6 +50,7 @@ function PricingTab(props: PricingTabProps) {
             )
           })}
         </ul>
+        
       </div>
     </div>
   )
@@ -57,23 +60,30 @@ export default function PricingTable() {
   const [yearly, setYearly] = useState<boolean>(true)
 
   return (
-    <div>
+    <div className=' bg-white'>
+      
+    <div className=" text-center py-20">
+      <div className="text-gray-800">
+        <h2 className="text-4xl font-bold md:text-5xl md:leading-none">Pay As You Grow</h2>
+      </div>
+      <p className="mx-auto mt-2  max-w-3xl text-gray-800">Explore Our Tailored Solutions: Find the Ideal Package for Your Business Needs.</p>
+    </div>
 
       {/* Pricing toggle */}
       <div className="flex justify-center max-w-[14rem] m-auto mb-8 lg:mb-16 ">
-        <div className="relative flex w-full p-1 bg-white dark:bg-slate-900 rounded-full">
+        <div className="relative flex w-full p-1 bg-gray-200 rounded-full">
           <span className="absolute inset-0 m-1 pointer-events-none" aria-hidden="true">
             <span className={`absolute inset-0 w-1/2 bg-indigo-500 rounded-full shadow-sm shadow-indigo-950/10 transform transition-transform duration-150 ease-in-out ${yearly ? 'translate-x-0' : 'translate-x-full'}`}></span>
           </span>
           <button
-            className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${yearly ? 'text-white' : 'text-slate-500 dark:text-slate-400'}`}
+            className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300`}
             onClick={() => setYearly(true)}
             // aria-pressed={isAnnual}
           >
-            Yearly <span className={`${yearly ? 'text-indigo-200' : 'text-slate-400 dark:text-slate-500'}`}>-20%</span>
+            Yearly <span className={`${yearly ? 'text-gray-700' : 'text-slate-400'}`}>-20%</span>
           </button>
           <button
-            className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 ease-in-out ${yearly ? 'text-slate-500 dark:text-slate-400' : 'text-white'}`}
+            className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 `}
             onClick={() => setYearly(false)}
             // aria-pressed={isAnnual}
           >
@@ -82,19 +92,20 @@ export default function PricingTable() {
         </div>
       </div>
 
-      <div className="max-w-sm mx-auto grid gap-6 lg:grid-cols-3 items-start lg:max-w-none">
+      <div className="max-w-sm mx-auto grid gap-6 px-20 lg:grid-cols-3 text-black items-start lg:max-w-none">
 
         {/* Pricing tab 1 */}
         <PricingTab
           yearly={yearly}
           planName="Essential"
-          price={{ yearly: 29, monthly: 35 }}
-          planDescription="There are many variations available, but the majority have suffered."
+          price={{ yearly: 20, monthly: 220 }}
+          planDescription="New businesses or individuals just starting their online store."
           features={[
-            'Unlimited placeholder texts',
-            'Consectetur adipiscing elit',
-            'Excepteur sint occaecat cupidatat',
-            'Officia deserunt mollit anim',
+            'Chatbots Assistants',
+            'Social Media Integration',
+            'Customizable checkout',
+            'Basic SEO features',
+            'Basic analytics and reporting',
           ]} />
 
         {/* Pricing tab 2 */}
@@ -102,29 +113,32 @@ export default function PricingTable() {
           yearly={yearly}
           popular={true}
           planName="Perform"
-          price={{ yearly: 49, monthly: 55 }}
-          planDescription="There are many variations available, but the majority have suffered."
+          price={{ yearly: 33, monthly: 396 }}
+          planDescription="Large businesses with high volumes of sales and traffic."
           features={[
-            'Unlimited placeholder texts',
-            'Consectetur adipiscing elit',
-            'Excepteur sint occaecat cupidatat',
-            'Officia deserunt mollit anim',
-            'Predefined chunks as necessary',
+            'Everything in Starter',
+            'Email marketing tools',
+            'Advanced analytics and reporting',
+            'Multi-language support',
+            'Higher product limit (e.g., up to 500 products)',
+            'Chat and email support',
           ]} />
 
         {/* Pricing tab 3 */}
         <PricingTab
           yearly={yearly}
           planName="Enterprise"
-          price={{ yearly: 79, monthly: 85 }}
+          price={{ yearly: 70, monthly: 840 }}
           planDescription="There are many variations available, but the majority have suffered."
           features={[
-            'Unlimited placeholder texts',
-            'Consectetur adipiscing elit',
-            'Excepteur sint occaecat cupidatat',
-            'Officia deserunt mollit anim',
-            'Predefined chunks as necessary',
-            'Free from repetition',
+            'Everything in Perform plan',
+            'Custom domain',
+            'up to 1000 users',
+            'Chatbots Assistants',
+            'Social Media Integration',
+            'Customizable checkout',
+            'Advanced SEO features',
+            'Integration with external services (CRM, ERP,Quickbooks)',
           ]} />
 
       </div>
