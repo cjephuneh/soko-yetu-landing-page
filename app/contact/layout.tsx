c31f6dc4d5df7ecae907857d1a51109405f950ae
cjephuneh <calebjephuneh@gmail.com>
2023-12-21 22:30:03 +0300

    upd

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navbar/>
+      {/* <Navbar/> */}
       <body className={inter.className}>{children}</body>
       {/* <Footer/> */}
     </html>
