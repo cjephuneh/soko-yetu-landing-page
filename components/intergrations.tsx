1c3e65d4d71358c2dba123a16ef905d908eba9b8
cjephuneh <calebjephuneh@gmail.com>
2024-01-07 00:13:56 +0300

    updates

--- components/intergrations.tsx
+++ components/intergrations.tsx
@@ -1,57 +1,61 @@
-import React from 'react';
-// import Faceboo
+import React from "react";
+import Facebook from "@/public/icons8-facebook.svg";
+import Quickbooks from "@/public/icons8-quickbooks-32.png";
+import Whatsapp from "@/public/icons8-whatsapp-24.png"
+import Shopify from "@/public/icons8-shopify-96.png"
+import Instagram from "@/public/icons8-instagram-48.png"
+import Image from "next/image";
 
 const CtaCard = () => {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 rounded bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4">
-        
         <div className="mt-10 lg:mt-0 lg:w-1/2">
           {/* Replace with your own image URL */}
-          <div className="flex flex-col">
+          <div className="flex flex-col space-y-4">
             {/* First Row */}
-            <div className="flex items-center">
-              <div className="social-media-item">
-                <img src="/images/social-media-1.svg" alt="Social Media 1" />
-                <span>Social Media 1</span>
+            <div className="flex items-center gap-4">
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Facebook} alt="Facebook" width={60} height={60}  />
+                <span>Facebook</span>
               </div>
-              <div className="social-media-item">
-                <img src="/images/social-media-2.svg" alt="Social Media 2" />
-                <span>Social Media 2</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Quickbooks} alt="Quickbooks" width={60} height={60}  />
+                <span>Quickbooks</span>
               </div>
-              <div className="social-media-item fade-effect">
-                <img src="/images/social-media-3.svg" alt="Social Media 3" />
-                <span>Social Media 3</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Whatsapp} alt="Whatsapp" width={60} height={60}  />
+                <span>Whatsapp</span>
               </div>
             </div>
             {/* Second Row */}
-            <div className="flex items-center">
-              <div className="social-media-item">
-                <img src="/images/social-media-4.svg" alt="Social Media 4" />
-                <span>Social Media 4</span>
+            <div className="flex items-center gap-4">
+            <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Shopify} alt="Shopify" width={60} height={60} />
+                <span>Shopify</span>
               </div>
-              <div className="social-media-item">
-                <img src="/images/social-media-5.svg" alt="Social Media 5" />
-                <span>Social Media 5</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Instagram} alt="Instagram" width={60} height={60}  />
+                <span>Instagram</span>
               </div>
-              <div className="social-media-item fade-effect">
-                <img src="/images/social-media-6.svg" alt="Social Media 6" />
-                <span>Social Media 6</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Facebook} alt="Facebook" width={60} height={60} />
+                <span>Facebook</span>
               </div>
             </div>
             {/* Third Row */}
-            <div className="flex items-center">
-              <div className="social-media-item">
-                <img src="/images/social-media-7.svg" alt="Social Media 7" />
-                <span>Social Media 7</span>
+            <div className="flex items-center gap-4">
+            <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Facebook} alt="Facebook" width={60} height={60}  />
+                <span>Facebook</span>
               </div>
-              <div className="social-media-item">
-                <img src="/images/social-media-8.svg" alt="Social Media 8" />
-                <span>Social Media 8</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Facebook} alt="Facebook" width={60} height={60} />
+                <span>Facebook</span>
               </div>
-              <div className="social-media-item fade-effect">
-                <img src="/images/social-media-9.svg" alt="Social Media 9" />
-                <span>Social Media 9</span>
+              <div className=" flex border rounded-xl p-2 items-center space-x-2 border-gray-700 bg-slate-900">
+                <Image src={Facebook} alt="Facebook" width={60} height={60} />
+                <span>Facebook</span>
               </div>
             </div>
           </div>
@@ -62,7 +66,8 @@ const CtaCard = () => {
             Connect with all the tools you already know and love
           </h1>
           <p className="mt-4 text-lg">
-            Connect Aidbase with a broad range of third-party tools and services.
+            Connect Aidbase with a broad range of third-party tools and
+            services.
           </p>
           <button className="mt-8 flex items-center justify-center px-6 py-3 bg-blue-700 rounded-md shadow hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-2">
             Coming soon
